799cc491e3c5f2fcbb3ebd50ffb1719db35c24a2
Nuwan Thuduwage <nuwan.thuduwage@autozone.com>
2025-04-27 11:59:42 -0400

    fix: initial commit (nodejs basics)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
+import fs from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = "fresh.txt";
+  const basePath = "src/fs/files";
+
+  try {
+    const files = await fs.readdir(basePath);
+    const isFileExists = files.includes(fileName);
+
+    if (files.length === 0) {
+      console.error("No Files in directory");
+      return;
+    }
+    
+    if (isFileExists) {
+      console.error("FS operation failed");
+      return;
+    }
+
+    try {
+      await fs.writeFile(`${basePath}/${fileName}`, "I am fresh and young");
+      console.log("File created successfully");
+    } catch (error) {
+      console.error("Error creating file:", error);
+    }
+  } catch (error) {
+    console.error("Error reading directory:", error);
+  }
 };
 
 await create();
