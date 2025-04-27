964dca9b0d4cd9dd8e78ce796823effd1954a518
Nuwan Thuduwage <nuwan.thuduwage@autozone.com>
2025-04-27 16:32:51 -0400

    finish cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const outputParts = [];
+
+  // Iterate through arguments
+  for (let i = 0; i < args.length; i += 2) {
+    const propNameArg = args[i];
+    const value = args[i + 1];
+
+    const propName = propNameArg.startsWith("--")
+      ? propNameArg.substring(2)
+      : propNameArg;
+    outputParts.push(`${propName} is ${value}`);
+  }
+
+  console.log(outputParts.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const rssVars = [];
+
+  // Iterate over all environment variables
+  for (const key in process.env) {
+    if (key.startsWith(prefix)) {
+      rssVars.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(rssVars.join("; "));
 };
 
 parseEnv();
