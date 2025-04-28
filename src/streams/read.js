c12f6f6515729ca5a837ba8ec9e82fb9cfb41e25
Nuwan Thuduwage <nuwan.thuduwage@autozone.com>
2025-04-28 15:06:01 -0400

    fix: modified read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,14 +1,23 @@
 import { createReadStream } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
 
 const read = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  
   // Create a readable stream for the target file
-  const readableStream = createReadStream("src/streams/files/fileToRead.txt");
+  const readableStream = createReadStream(filePath);
 
   readableStream.on("error", (error) => {
     console.error(`Error reading file: ${error.message}`);
   });
+  
+  readableStream.on("end", () => {
+    console.log("\nFinished reading the file");
+  });
 
-  // Pipe the data from the readable stream directly to the standard output stream
   readableStream.pipe(process.stdout);
 };
 
